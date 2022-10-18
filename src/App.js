b11582a0c1155b9628ad40517257de73a1bb3899
pari3266 <parisatalebi25@gmail.com>
2022-10-18 15:43:41 +0330

    02 tut react

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
+
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [bgColor, setbgColor] = useState('');
+  const handleColor = (e) => {
+    setbgColor(e.target.value)
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className='s' style={{backgroundColor: bgColor}}>
+
+      </div>
+      <input placeholder='Add color name' onChange={(e) => handleColor(e)}/>
     </div>
   );
 }
